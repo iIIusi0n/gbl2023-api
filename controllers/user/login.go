0378e6d4c3e0e386eb08d68f4a8f2e0f4a3b272f
iiiusi0n <iiiusi0n@wyvern.pw>
2023-06-23 18:18:37 +0900

    Fix error handling in IsUserExist

--- controllers/user/login.go
+++ controllers/user/login.go
@@ -2,11 +2,20 @@ package user
 
 import (
 	"gbl-api/data"
+	"gorm.io/gorm"
+	"log"
 )
 
 func IsUserExist(uid string) bool {
 	db := data.GetDatabase()
 	var user User
-	db.Where("uid = ?", uid).First(&user)
-	return user.UID != ""
+	err := db.Where("uid = ?", uid).First(&user).Error
+	if err == gorm.ErrRecordNotFound {
+		return false
+	} else if err == nil {
+		return true
+	} else {
+		log.Println(err)
+		return false
+	}
 }
